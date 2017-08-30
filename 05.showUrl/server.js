684ecddd947b5129e1e2f2f18973f7779182b2fd
mitoo <mitoolab@gmail.com>
2017-08-30 17:14:47 +0900

    shotUrl Project

---
+++ 05.showUrl/server.js
@@ -0,0 +1,68 @@
+// 2017 JavaScript Mitoo all rights reserved.
+
+const express = require('express')
+const morgan = require('morgan')
+const basicAuth = require('express-basic-auth')
+const randomstring = require("randomstring")
+const bodyParser = require('body-parser')
+
+const data = [
+  {
+    longUrl: 'http://google.com', 
+    id: randomstring.generate(6)
+  }
+]
+// 짧은 URL 변경
+// 302 응답
+
+const app = express()
+
+const authMiddleware = basicAuth({
+  users: { 'admin': 'admin' },
+  challenge: true,
+  realm: 'Imb4T3st4pp'
+})
+
+const bodyParserMiddleware = bodyParser.urlencoded({ extended: false })
+
+app.set('view engine', 'ejs')
+// static 설정
+app.use('/static', express.static('public'))
+// tiny : 기본 설정
+app.use(morgan('tiny'))
+
+app.get('/', authMiddleware, (req, res) => {
+  res.render('index.ejs', {data})
+})
+
+app.get('/:id', (req, res) => {
+  const id = req.params.id
+  const matched = data.find(item => item.id === id)
+  if (matched) {
+    res.redirect(301, matched.longUrl)
+  } else {
+    res.status(404)
+    res.send('404 Not Found')
+  }
+})
+
+
+
+app.post('/', authMiddleware, bodyParserMiddleware, (req,res) => {
+  const longUrl = req.body.longUrl
+  let id
+  while(true) {
+    const candidate = randomstring.generate(6)
+    const matched = data.find(item => item.id === candidate)
+    if (!matched) {
+      id = candidate
+      break
+    }
+  }
+  data.push({id, longUrl})
+  res.redirect('/')
+})
+
+app.listen(3000, () => {
+  console.log('listening....:');
+})
